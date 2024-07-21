cca56ff8fe25d2e2730ed939637a70e5d29ef902
Patrice DARGENTON <patrice.dargenton@free.fr>
2024-07-21 12:31:19 +0200

    Option Strict On

--- Src/modUtilFile.vb
+++ Src/modUtilFile.vb
@@ -136,9 +136,9 @@ Retry:
         End Using
 
         ' Analyze the BOM
-        If bom(0) = &H2B AndAlso bom(1) = &H2F AndAlso bom(2) = &H76 Then
-            Return Encoding.UTF7
-        End If
+        'If bom(0) = &H2B AndAlso bom(1) = &H2F AndAlso bom(2) = &H76 Then
+        '    Return Encoding.UTF7
+        'End If
         If bom(0) = &HEF AndAlso bom(1) = &HBB AndAlso bom(2) = &HBF Then
             Return Encoding.UTF8
         End If
